02dd3b0e6894fc19292ebbee4ca1c5cd3f4d2830
Siello Riatto <siello.riatto@gmail.com>
2023-12-29 15:45:47 -0300

    Added initial pages (clients, client, project)

--- src/app/[client]/page.tsx
+++ src/app/[client]/page.tsx
@@ -1,7 +1,7 @@
-export default function Clients() {
+export default function Client() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Clients</div>
+      <div>Client</div>
     </main>
   )
 }
