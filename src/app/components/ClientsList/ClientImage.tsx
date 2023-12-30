391365ba0d9830fbba2e46bb4f7a62a6405e3609
Siello Riatto <siello.riatto@gmail.com>
2023-12-29 23:26:17 -0300

    Added ClientList, ClientItem and ClientImage components

---
+++ src/app/components/ClientsList/ClientImage.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react"
+
+interface ClientImageProps {
+	imageName?: string
+}
+
+export default function ClientImage({
+	imageName,
+}: ClientImageProps) {
+	const imageWrapClassName = useMemo(() => (`
+		w-12 h-12 p-[0.1875rem]
+		rounded-full ring-1 ring-slate-900/10
+		shadow overflow-hidden flex-none
+		dark:bg-slate-600 dark:highlight-white/20
+	`), [])
+	
+	const imageUrl = useMemo(() => (`
+		url(/images/${imageName || 'component-driven.82a66c3c.png'})
+	`), [imageName])
+	
+	return (
+		<div className={imageWrapClassName}>
+			<div
+				className="aspect-w-1 aspect-h-1 bg-[length:100%] dark:hidden"
+				style={{
+						backgroundImage: imageUrl,
+						width: `100%`,
+						height: `100%`
+				}}
+			></div>
+		</div>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import dynamic from "next/dynamic"
 import Header from "./components/Header"
+import Loader from "./components/Loader"
 
+const ClientsList = dynamic(
+  () => import('./components/ClientsList'),
+  {
+    ssr: false,
+    loading: Loader
+  }
+)
 
 export default function Clients() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <Header pageName={"Clients"} />
+      <ClientsList />
     </main>
   )
 }
